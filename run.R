4ff518d26cf7ad99ea3d2ca8e51875db8edbcc54
easella <74786054+easella@users.noreply.github.com>
2022-04-24 20:50:19 -0400

    Update run.R

--- run.R
+++ run.R
@@ -1,16 +1,7 @@
 library(shiny)
 install.packages("shinyBS")
 install.packages("rvest")
-install.packages("plyr")
-install.packages("dplyr")
-install.packages("shinycustomloader")
-install.packages("DT")
-install.packages("shinycssloaders")
-install.packages("profvis")
-install.packages("ggplot2")
-install.packages("ggthemes")
-install.packages("colorfindr")
-install.packages("shinyalert")
+
 library(rvest)
 
 install.packages("shinythemes")
